a8a93cba83f0bf3f6b1b954a6b28ed7ea66a3d56
rhys-cmd <81196117+rhys-cmd@users.noreply.github.com>
2022-02-17 13:08:31 +1300

    Rhys, 17/2

--- arcadegame.py
+++ arcadegame.py
@@ -4,27 +4,45 @@ import arcade
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 650
 SCREEN_TITLE = "Platformer"
-
+CHARACTER_SCALING = 1
 
 class MyGame(arcade.Window):
     """
     Main application class.
     """
-
     def __init__(self):
 
         # Call the parent class and set up the window
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE, resizable=True)
 
         arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
-
+    
+    def on_resize(self, SCREEN_WIDTH, SCREEN_HEIGHT):
+        super().on_resize(SCREEN_HEIGHT, SCREEN_WIDTH)
+    
+    
+    
+    
+    
     def setup(self):
         """Set up the game here. Call this function to restart the game."""
+        image_source = ("New Piskel.png")
+        self.player_list = arcade.SpriteList()
+        self.wall_list = arcade.SpriteList(use_spatial_hash=True)
+        self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
         self.player_list = arcade.SpriteList()
         self.wall_list = arcade.SpriteList(use_spatial_hash=True)
 
+        # Set up the player, specifically placing it at these coordinates.
+        
+        self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
+        self.player_sprite.center_x = 64
+        self.player_sprite.center_y = 128
+        self.player_list.append(self.player_sprite)
+
     def on_draw(self):
         """Render the screen."""
+        self.player_list.draw()
 
         self.clear()
         # Code to draw the screen goes here
